005b37e9cc26dfddb74032d56c85fce0fd810c73
Ondrej Polesny <ondrejp@kontent.ai>
2023-04-27 15:29:45 +0200

    Removes unused import

--- src/ChatGPTApp.tsx
+++ src/ChatGPTApp.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Configuration, OpenAIApi } from "openai";
 
 export const ChatGPTApp: FC = () => {
